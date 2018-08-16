b42047489e632ffc3d1125f4ba3f3f64e654b945
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2018-08-16 18:19:07 +0200

    Labstack echo

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,18 +2,19 @@ package routes
 
 import (
 	"github.com/labstack/echo"
-	"github.com/mozey/gateway/internal/controllers"
+	"github.com/mozey/gateway/internal/handlers"
 	"github.com/mozey/gateway/internal/middleware"
 )
 
+// CreateMux creates a new instance of echo
 func CreateMux() *echo.Echo {
 	e := echo.New()
 
 	middleware.Setup(e)
 
-	e.GET("/v1", controllers.Index)
-	e.GET("/v1/foo", controllers.Foo)
-	e.GET("/v1/bar", controllers.Bar)
+	e.GET("/v1", handlers.Index)
+	e.GET("/v1/foo", handlers.Foo)
+	e.GET("/v1/bar", handlers.Bar)
 
 	return e
 }
